eb41e2952875afe0d3a4a46e6cb065ed38150656
Zach Bonham <zachbonham@gmail.com>
2021-12-28 09:26:08 -0600

    Random Knight selection

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -1,5 +1,5 @@
 import Phaser from "phaser";
-
+import {KnightType} from '../types/KnightType'
 export default class Preloader extends Phaser.Scene
 {
     constructor()
@@ -7,22 +7,28 @@ export default class Preloader extends Phaser.Scene
         super("preloader");
     }
 
+    init() 
+    {
+        console.log("PreloaderScene init");
+    }
+
     preload()
     {
-        //this.load.image("tiles", "./tiles/dungeon-01.png");
-        //this.load.image("tiles", "./tiles/dungeon-02.png");
         this.load.image("tiles", "./tiles/dungeon-03.png");
-        this.load.image("combat-tiles", "./tiles/dungeon-03.png");
+        this.load.image("combat_tiles", "./tiles/dungeon-01.png")
 
-
-        //this.load.tilemapTiledJSON("dungeon", "./tiles/dungeon-01.json");
-        //this.load.tilemapTiledJSON("dungeon", "./tiles/dungeon-02.json");
         this.load.tilemapTiledJSON("dungeon", "./tiles/dungeon-03.json");
-        this.load.tilemapTiledJSON("combat", "./tiles/dungeon-03.json");
 
-        this.load.atlas("knight1", "./characters/knight.png", "./characters/knight_atlas.json");
-        //this.load.atlas("knight1", "./characters/knight-green.png", "./characters/knight_atlas.json");
+        let numberOfKnights = Object.keys(KnightType).length / 2;
 
+        let knightSelection = Phaser.Math.Between(0,numberOfKnights-1)
+        let knightType = KnightType[knightSelection].toLowerCase()
+
+        console.log(`selected ${knightType} knight`)
+
+        this.load.tilemapTiledJSON("combat", "./tiles/combat-01.json");
+        
+        this.load.atlas("knight1", `./characters/knight-${knightType}.png`, "./characters/knight_atlas.json");
 
     }
 
---
+++ src/types/KnightType.ts
@@ -0,0 +1,11 @@
+
+enum KnightType {
+    GRAY,
+    PURPLE,
+    BLUE,
+    GREEN
+}
+
+export {
+    KnightType
+}
